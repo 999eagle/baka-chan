64075b59472c5a7b763482540d7634ea7ac96401
999eagle <erik@tauchert.de>
2016-10-14 20:06:41 +0200

    dev commands work now

--- baka-chan/cmd_dev.py
+++ baka-chan/cmd_dev.py
@@ -4,10 +4,12 @@ import globals
 from util import *
 
 async def cmd_dev(message, args):
-	if len(args) == 1:
-		if args[0] == 'disable':
+	if len(args) == 0 or args[0] != 'dev':
+		return
+	if len(args) == 2:
+		if args[1] == 'disable':
 			globals.disabled = True
-			await globals.client.change_presence(game = None, status = discord.Status.idle)
+			await globals.client.change_presence(game = discord.Game(name = 'Currently disabled'), status = discord.Status.idle)
 			await send_message(message.channel, 'Baka-chan was disabled.')
 		elif args[1] == 'enable':
 			globals.disabled = False
