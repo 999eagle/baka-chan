ac6c1a2757e2b531c3fd7179e531d7038f444ccb
999eagle <erik@tauchert.de>
2016-10-16 11:36:46 +0200

    changed image stuff
    no gifs as direct upload atm, all images for direct upload are now less
    than 40kb
    images without link will now also be sent as direct upload even with
    send_directly = False

--- baka-chan/util.py
+++ baka-chan/util.py
@@ -48,49 +48,31 @@ async def send_message(channel, message):
 		log.log_warning('Forbidden to speak in channel {0}({1}) on {2}({3})'.format(channel.name, channel.id, channel.server.name, channel.server.id))
 		return False
 
-image_counts = {'gtfo': 3, 'thumbs_up': 4, 'wtf': 4, 'lewdapprove': 2}
-images = {'pay_respects':('pay_respects.jpg',''),
-          'poke':('poke.jpg','http://s15.postimg.org/a2515gqzf/poke.jpg'),
+image_counts = {'fu': 2, 'gtfo': 3, 'thumbs_up': 4, 'wtf': 4}
+images = {'boom':('','https://giphy.com/gifs/computer-gZBYbXHtVcYKs'),
           'calmdown':('calmdown.jpg',''),
           'cover_up':('cover_up.jpg',''),
+          'fu1':('fu1.jpg',''),
+          'fu2':('fu2.jpg',''),
           'gtfo1':('gtfo1.jpg','http://s12.postimg.org/p2prv7c3x/gtfo1.jpg'),
           'gtfo2':('gtfo2.jpg',''),
-          'gtfo3':('gtfo3.png',''),
-          'strip':('strip.jpg',''),
-          'thumbs_up1':('thumbs1.png',''),
+          'gtfo3':('gtfo3.jpg',''),
+          'lewd':('lewd.jpg',''),
+          'lewdapprove':('lewdbutiapprove.jpg',''),
+          'notwork':('notwork.jpg','http://i.imgur.com/CA1RMf7.png'),
+          'pay_respects':('pay_respects.jpg',''),
+          'poke':('poke.jpg','http://s15.postimg.org/a2515gqzf/poke.jpg'),
+          'thumbs_up1':('thumbs1.jpg',''),
           'thumbs_up2':('thumbs2.jpg',''),
           'thumbs_up3':('thumbs3.jpg',''),
           'thumbs_up4':('thumbs4.jpg',''),
+          'trustme':('trustme.jpg',''),
           'wtf1':('wtf1.jpg',''),
           'wtf2':('wtf2.jpg',''),
-          'wtf3':('wtf3.png',''),
-          'wtf4':('wtf4.png',''),
-          'wtf5':('wtf5.gif',''),
+          'wtf3':('wtf3.jpg',''),
+          'wtf4':('wtf4.jpg',''),
           'you_aint_kawaii':('You aint kawaii.jpg',''),
-          'lewd':('lewd.jpg',''),
-          'boom':('','https://giphy.com/gifs/computer-gZBYbXHtVcYKs'),
-          'notwork':('notwork.png','http://i.imgur.com/CA1RMf7.png'),
-          'trustme':('trustme.png',''),
-          'silver':('silver.gif',''),
-          'slap':('slap1.gif',''),
-          'rainbow':('animurainbow.gif',''),
-          'fu1':('fu1.gif',''),
-          'fu2':('fu2.gif',''),
-          'fu3':('fu3.gif',''),
-          'fu4':('fu4.png',''),
-          'fu5':('fu5.jpg',''),
-          'fu6':('fu6.gif',''),
-          'fu7':('fu7.gif',''),
-          'fu8':('fu8.gif',''),
-          'fu9':('fu9.gif',''),
-          'lewdapprove1':('lewdbutiapprove1.png',''),
-          'lewdapprove2':('lewdbutiapprove2.jpg',''),
-          'nope1':('nope1.jpg',''),
-          'nope2':('nope2.gif',''),
-          'nope3':('nope3.png',''),
-          'nope4':('nope4.gif',''),
-          'party1':('partyhard1.gif',''),
-          'party2':('partyhard2.gif','')}
+          }
 
 async def send_image(channel, image):
 	if isinstance(channel, discord.Channel):
@@ -102,13 +84,12 @@ async def send_image(channel, image):
 		raise NameError('No image with that key found')
 	try:
 		data = images[image]
-		if send_directly and data[0] != '':
+		if (send_directly and data[0] != '') or data[1] == '':
 				await globals.client.send_file(channel, PlatformSpecific.inst().convert_path('img\\' + data[0]))
 				return True
 		else:
-			if data[1] != '':
-				await globals.client.send_message(channel, data[1])
-				return True
+			await globals.client.send_message(channel, data[1])
+			return True
 	except discord.errors.Forbidden:
 		log.log_warning('Forbidden to speak in channel {0}({1}) on {2}({3})'.format(channel.name, channel.id, channel.server.name, channel.server.id))
 	return False
